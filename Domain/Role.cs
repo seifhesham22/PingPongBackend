d8b1119b058f3f751b266de978a1404a932ad072
Seif Elmoazen <seifhesham882@gmail.com>
2026-07-31 22:37:16 +0700

    Add EF Core persistence layer and finalize domain model
    - Add PingPongDbContext and DesignTimeFactory under Data/
    - Add initial migration and model snapshot
    - Fix ChatMemeber.cs typo to  ChatMember.cs
    - Update Chat, Message, Role and User domain entities.

--- Domain/Role.cs
+++ Domain/Role.cs
@@ -1,4 +1,20 @@
 ﻿namespace PingPong.API.Domain
 {
-    public class Role { }
+    public class Role
+    {
+        public Guid Id { get; set; }
+        public Permissions Permissions { get; set; }
+        public bool CanJoinVoice { get; private set; }
+        public int Position { get; set; }
+        public Guid ServerId { get; set; }
+        public List<UserServer> Members = new List<UserServer>();
+    }
+    public enum Permissions
+    {
+        None = 0,
+        Read = 1 << 0,
+        Write = 1 << 1,
+        Delete = 1 << 2,
+        Admin = 1 << 3
+    }
 }
